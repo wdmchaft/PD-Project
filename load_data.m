9dc59e4b5f52cb56a842743d8d0cae6fa62d773b
Adriaan <arandia.t@gmail.com>
2009-10-12 15:03:06 -0700

    This now positions the display window for easy viewing.

--- load_data.m
+++ load_data.m
@@ -1,12 +1,13 @@
 %cd "/home/adriaan/Homework/EECE 496/Starting Data/Pilot03";
-Platform = load('Pilot03\Pilot03_Platform_Position_fwdSHRT.txt');
-Knee = load('Pilot03\Pilot03_Knee_Angles_fwdSHRT.txt');
-Ankle = load('Pilot03\Pilot03_Ankle_Angles_fwdSHRT.txt');
-Hip = load('Pilot03\Pilot03_Hip_Angles_fwdSHRT.txt');
-Shoulder = load('Pilot03\Pilot03_Shoulder_Angles_fwdSHRT.txt');
-Trunk = load('Pilot03\Pilot03_Trunk_Angles_fwdSHRT.txt');
-Elbow = load('Pilot03\Pilot03_Elbow_Angles_fwdSHRT.txt');
-Neck = load('Pilot03\Pilot03_Neck_Angles_fwdSHRT.txt');
+Data = 'Pilot03';
+Platform = load(strcat(Data,'\',Data,'_Platform_Position_fwdSHRT.txt'));
+Knee = load(strcat(Data,'\', Data, '_Knee_Angles_fwdSHRT.txt'));
+Ankle = load(strcat(Data, '\', Data, '_Ankle_Angles_fwdSHRT.txt'));
+Hip = load(strcat(Data, '\', Data, '_Hip_Angles_fwdSHRT.txt'));
+Shoulder = load(strcat(Data, '\', Data, '_Shoulder_Angles_fwdSHRT.txt'));
+Trunk = load(strcat(Data, '\', Data, '_Trunk_Angles_fwdSHRT.txt'));
+Elbow = load(strcat(Data, '\', Data, '_Elbow_Angles_fwdSHRT.txt'));
+Neck = load(strcat(Data, '\', Data, '_Neck_Angles_fwdSHRT.txt'));
 
 % note same time vector is the first column of all data sets
 % number of trials depends on trial type:
@@ -21,10 +22,14 @@ Joints.Trial3 = [Ankle(:,4) Elbow(:,4) Hip(:,4) Knee(:,4) Neck(:,4) Shoulder(:,4
 Joints.Trial4 = [Ankle(:,5) Elbow(:,5) Hip(:,5) Knee(:,5) Neck(:,5) Shoulder(:,5) Trunk(:,5)];
 Joints.Trial5 = [Ankle(:,6) Elbow(:,6) Hip(:,6) Knee(:,6) Neck(:,6) Shoulder(:,6) Trunk(:,6)];
 
-figure();
-plot(time, Joints.Trial1); legend('Ankle','Elbow','Hip','Knee','Neck','Shoulder','Trunk');
-figure();
-plot(time, Platform(:,2));
+screen_size = get(0, 'ScreenSize')
+% Syntax is [xstart, ystart, width, height]. For some reason, it still
+% overshoots...
+figure('Position', [4 screen_size(4)/3-75 2*screen_size(3)/3 2*screen_size(4)/3]);
+subplot(2,1,1);
+plot(time, Joints.Trial2); legend('Ankle','Elbow','Hip','Knee','Neck','Shoulder','Trunk');
+subplot(2,1,2);
+plot(time, Platform(:,2)); legend('Platform Position');
 %figure();
 %plot(time, Joints.Trial2); legend('Ankle','Elbow','Hip','Knee','Neck','Shoulder','Trunk');
 %figure();
